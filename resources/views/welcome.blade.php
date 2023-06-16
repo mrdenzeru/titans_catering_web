f3da6f6cb3750089ed66f16a087cd3d6b74de3f0
mrdenzeru <ddaveutrera23@gmail.com>
2023-06-16 20:53:35 +0800

    new welcome page with css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,13 +12,15 @@
         {{-- bootstrap-css --}}
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
+        {{-- manual-css --}}
+        <link rel="stylesheet" href="{{ asset('assets/css/welcome-css/main.css') }}">
 
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
             }
 
-            .relative a {
+            a {
                 text-decoration: none;
                 color: black;
                 font-weight: 700;
@@ -26,80 +28,95 @@
 
             .carousel h5, p {
                 color: white;
-                font-weight: 700
-            }
-
-            .carousel-control-prev {
-                color: white;
+                font-w
             }
         </style>
 
     </head>
-    <body class="">
-        <div class="relative flex items-top justify-center">
-            <div class="float-start fixed px-6 py-4 sm:block mx-3">
-                <button type="button" class="btn btn-info">
-                    <a href="{{ url('welcome') }}" class="text-lg">TITANS CATERING</a>
-                </button>
-            </div>
-            
-            @if (Route::has('login'))
-                <div class="fixed px-6 py-4 sm:block float-end mx-3">
+    <body class="antialiased">
+        <header id="header" class="header fixed-top d-flex align-items-center">
+            <div class="container d-flex align-items-center justify-content-between">
+        
+              <a href="{{ url('/') }}" class="logo d-flex align-items-center me-auto me-lg-0">
+                <h1>TitansCatering<span>.</span></h1>
+              </a>
+        
+              <nav id="navbar" class="navbar">
+                <ul>
+                  <li><a href="#hero">Home</a></li>
+                  <li><a href="#about">About</a></li>
+                  <li><a href="#menu">Menu</a></li>
+                  <li><a href="#events">Events</a></li>
+                  <li><a href="#chefs">Chefs</a></li>
+                  <li><a href="#gallery">Gallery</a></li>
+                  <li><a href="#contact">Contact</a></li>
+                </ul>
+              </nav><!-- .navbar -->
+        
+              @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
                         <button type="button" class="btn btn-primary">
-                            <a href="{{ url('/home') }}" class="text-lg">Home</a>
+                            <a href="{{ url('/home') }}" class="btn-book-a-table">Home</a>
                         </button>
                     @else
-                        <button type="button" class="btn btn-success">
-                            <a href="{{ route('login') }}" class="text-lg">Log in</a>
-                        </button>
+
+                        <a href="{{ route('login') }}" class="btn-book-a-table">Log in</a>
+
                         @if (Route::has('register'))                       
-                            <button type="button" class="btn btn-warning">
-                                <a href="{{ route('register') }}" class="text-lg">Register</a>
-                            </button>
+
+                            <a href="{{ route('register') }}" class="mx-1">Register</a>
+
                         @endif
                     @endauth
                 </div>
             @endif
-        </div>
+              <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+              <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+        
+            </div>
+          </header><!-- End Header -->
+        <div class="relative flex items-top justify-center ">
+            
 
-        <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-              <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                  <div class="carousel-item active" data-bs-interval="10000">
+                    <img src="{{ asset('assets/img/1.png') }}" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5>First slide label</h5>
+                      <p>Some representative placeholder content for the first slide.</p>
+                    </div>
+                  </div>
+                  <div class="carousel-item" data-bs-interval="2000">
+                    <img src="{{ asset('assets/img/2.png') }}" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5>Second slide label</h5>
+                      <p>Some representative placeholder content for the second slide.</p>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="{{ asset('assets/img/3.png') }}" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                      <h5>Third slide label</h5>
+                      <p>Some representative placeholder content for the third slide.</p>
+                    </div>
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
             </div>
-            <div class="carousel-inner">
-              <div class="carousel-item active" data-bs-interval="10000">
-                <img src="{{ asset('assets/img/1.png') }}" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                  <h5>First slide label</h5>
-                  <p>Some representative placeholder content for the first slide.</p>
-                </div>
-              </div>
-              <div class="carousel-item" data-bs-interval="2000">
-                <img src="{{ asset('assets/img/2.png') }}" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                  <h5>Second slide label</h5>
-                  <p>Some representative placeholder content for the second slide.</p>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <img src="{{ asset('assets/img/3.png') }}" class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                  <h5>Third slide label</h5>
-                  <p>Some representative placeholder content for the third slide.</p>
-                </div>
-              </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
-            </button>
         </div>
 
 
